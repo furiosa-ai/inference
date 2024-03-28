0990bc088a254c4a3b538d847656ec3b50bb6f7f
jason-furiosa <jason@furiosa.ai>
2024-03-28 04:16:30 +0000

    refactor: remove additional features in main.py

--- language/gpt-j/main.py
+++ language/gpt-j/main.py
@@ -6,10 +6,6 @@ import sys
 from backend import get_SUT
 sys.path.insert(0, os.getcwd())
 
-import time
-from datetime import timedelta
-from pathlib import Path
-
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -19,7 +15,7 @@ def get_args():
                         "Server"], default="Offline", help="Scenario")
     parser.add_argument("--model-path", default="EleutherAI/gpt-j-6B", help="")
     parser.add_argument(
-        "--dataset-path", type=Path, default="./data/cnn_eval.json", help="")
+        "--dataset-path", default="./data/cnn_eval.json", help="")
     parser.add_argument(
         "--calib-dataset-path", default="./data/cnn_dailymail_calibration.json", help="")
     parser.add_argument("--accuracy", action="store_true",
@@ -39,9 +35,9 @@ def get_args():
                         help="user config for user LoadGen settings such as target QPS")
     parser.add_argument("--max_examples", type=int, default=None,
                         help="Maximum number of examples to consider (not limited by default)")
-    parser.add_argument("--quant_config_path", type=Path, help="a config for model quantization")
-    parser.add_argument("--quant_param_path", type=Path, help="quantization parameters for calibraed layers")
-    parser.add_argument("--quant_format_path", type=Path, help="quantization specifications for calibrated layers")
+    parser.add_argument("--quant_config_path", help="a config for model quantization")
+    parser.add_argument("--quant_param_path", help="quantization parameters for calibraed layers")
+    parser.add_argument("--quant_format_path", help="quantization specifications for calibrated layers")
     parser.add_argument("--quantize", action="store_true", help="quantize model using ModelComPressor(MCP)")
     parser.add_argument('--torch_optim', default='default', type=str, choices=['default', 'none'], help='Torch optimization')
     parser.add_argument("--num_splits", type=int, default=1, help="")
@@ -93,21 +89,7 @@ def main():
         settings.mode = lg.TestMode.PerformanceOnly
     log_path = os.environ.get("LOG_PATH")
     if not log_path:
-        dataset_filename = args.dataset_path.stem
-        quant_config_filename = args.quant_config_path.stem
-        if args.quant_config_path is not None:
-            if "fp32" in args.quant_config_path.as_posix() or args.quantize == False:
-                if args.num_splits > 1:
-                    log_path = f"build/logs/fp32/{dataset_filename}_{args.num_splits}_{args.split_idx}"
-                else:
-                    log_path = f"build/logs/fp32/{dataset_filename}"
-            else:
-                if args.num_splits > 1:
-                    log_path = f"build/logs/{quant_config_filename}/{dataset_filename}_{args.num_splits}_{args.split_idx}"
-                else:
-                    log_path = f"build/logs/{quant_config_filename}/{dataset_filename}"
-        else:
-            log_path = f"build/logs/{dataset_filename}"
+        log_path = "build/logs"
     if not os.path.exists(log_path):
         os.makedirs(log_path)
     log_output_settings = lg.LogOutputSettings()
@@ -116,13 +98,8 @@ def main():
     log_settings = lg.LogSettings()
     log_settings.log_output = log_output_settings
     log_settings.enable_trace = True
-    start_time = time.time()
-    lg.StartTestWithLogSettings(sut.sut, sut.qsl, settings, log_settings, args.audit_conf)
 
-    end_time = time.time()
-    time_seconds = end_time - start_time
-    time_hour = timedelta(seconds=time_seconds)
-    print(f"Test running time: {time_hour}")
+    lg.StartTestWithLogSettings(sut.sut, sut.qsl, settings, log_settings, args.audit_conf)
     print("Test Done!")
 
     print("Destroying SUT...")
