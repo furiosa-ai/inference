7d88c4e1205322bd022d95ebde5a222ab29dc734
BeomGeunCho <beomgeun.cho@furiosa.ai>
2024-07-10 08:43:41 +0000

    set default values

--- language/bert/quantization/quantize.py
+++ language/bert/quantization/quantize.py
@@ -5,6 +5,8 @@ import model_compressor  # isort:skip
 
 from quantization.utils import get_kwargs  # isort:skip
 
+TARGET_MACHINE = 'RGDA0'
+QLEVEL = 4
 
 def quantize_model(
     model: GraphModule, qparam_path: str, qformat_path: str
@@ -16,6 +18,6 @@ def quantize_model(
         model,
         qformat_path=qformat_path,
         qparam_path=qparam_path,
-        target_machine='RGDA0',
-        qlevel=4,
+        target_machine=TARGET_MACHINE,
+        qlevel=QLEVEL,
     )
