bd8b31405b851d67cad73ce90c3c472723517228
sunghyuckhong <sunghyuck.hong@furiosa.ai>
2024-07-15 07:51:33 +0000

    add llama ci files for calib test

--- ci_scripts/qllama2-70b_calib_test.sh
+++ ci_scripts/qllama2-70b_calib_test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # define env. variables
-model_name=qgpt-j
-model_dir=language/gpt-j
+model_name=qllama2-70b
+model_dir=language/llama2-70b
 git_dir=$(git rev-parse --show-toplevel)
 work_dir=$git_dir/$model_dir
 data_dir=$git_dir/data
-quant_data_dir=$data_dir/quantization/gpt-j
+quant_data_dir=$data_dir/quantization/llama2-70b
 log_dir=$git_dir/logs
 env_name=mlperf-$model_name
 conda_base=$($CONDA_EXE info --base)
@@ -19,19 +19,22 @@ source "$conda_base/etc/profile.d/conda.sh"
 conda activate $env_name
 
 # eval model
-printf "\n============= STEP-1: Run calibration =============\n"
+printf "\n============= STEP-4: Run eval =============\n"
 SCENARIO=${SCENARIO:="Offline"}
-#BACKEND="rngd"
-MODEL_TYPE="golden"
-MODEL_PATH=$data_dir/models/gpt-j
-DATASET_PATH=$data_dir/dataset/cnn-daily-mail/validation/cnn_eval.json
-LOG_PATH=$log_dir/$model_name/$SCENARIO/$(date +%Y%m%d_%H%M%S%Z)
-N_COUNT=${N_COUNT:="13368"} # total_len=13,368
+BACKEND="rngd"
+DATA_TYPE=${DATA_TYPE:="float32"}
+N_COUNT=${N_COUNT:="24576"} # total_len = 24,576
+DEVICE=${DEVICE:="cuda:0"}
 
+if [ $DEVICE = "cpu" ];
+    then DATA_TYPE=float32;
+fi
 # quantization args
 export CALIBRATE=true
-N_CALIB=${N_CALIB:=1000} # total_len=1,000
-CALIB_DATA_PATH=$data_dir/dataset/cnn-daily-mail/calibration/cnn_dailymail_calibration.json
+#N_CALIB=${N_CALIB:=1000} # total_len=1,000
+
+export N_CALIB=4 #test code
+CALIB_DATA_PATH=$data_dir/dataset/open-orca/calibration/open_orca_gpt4_tokenized_llama.calibration_1000.pkl
 QUANT_CONFIG_PATH=$quant_data_dir/quant_config.yaml
 QUANT_PARAM_PATH=$quant_data_dir/calibration_range/quant_param.npy
 QUANT_FORMAT_PATH=$quant_data_dir/calibration_range/quant_format.yaml
@@ -40,33 +43,34 @@ printf "<<EVAL_CONFIG>>\n"
 printf "\tSCENARIO: $SCENARIO\n"
 printf "\tNUM_EVAL_DATA: $N_COUNT\n"
 printf "\tCALIBRATE: $CALIBRATE\n"
+printf "\tDEVICE: $DEVICE\n"
+
+CHECKPOINT_PATH=$data_dir/models/llama2/Llama-2-70b-chat-hf
+DATASET_PATH=$data_dir/dataset/open-orca/validation/open_orca_gpt4_tokenized_llama.sampled_24576.pkl
+LOG_PATH=$log_dir/$model_name/$SCENARIO/W8A8KV8/$(date +%Y%m%d_%H%M%S%Z)
 
 export LOG_PATH
 
 mkdir -p $LOG_PATH/calibration_range
 
 if [ "$CALIBRATE" = true ]; then
-    printf "\tNUM_CALIB_DATA: $N_CALIB\n"
-    QUANT_PARAM_PATH=$LOG_PATH/calibration_range/quant_param.npy
-    QUANT_FORMAT_PATH=$LOG_PATH/calibration_range/quant_format.yaml
-    python -m quantization.calibrate --model_path=$MODEL_PATH \
+    printf "\t\tNUM_CALIB_DATA: $N_CALIB\n"
+    python -m quantization.calibrate --backend=pytorch \
+                                     --model_path=$CHECKPOINT_PATH \
                                      --quant_config_path=$QUANT_CONFIG_PATH \
                                      --quant_param_path=$QUANT_PARAM_PATH \
                                      --quant_format_path=$QUANT_FORMAT_PATH \
                                      --calib_data_path=$CALIB_DATA_PATH \
                                      --n_calib=$N_CALIB \
-                                     --gpu
-    printf "Save calibration range to $LOG_PATH/calibration_range"
-else
-    cp $QUANT_PARAM_PATH $LOG_PATH/calibration_range/quant_param.npy
-    cp $QUANT_FORMAT_PATH $LOG_PATH/calibration_range/quant_format.yaml
-fi
+                                     --model_source $MODEL_SOURCE \
+                                     --gpu=True\
 
+fi
 
 printf "\n============= STEP-2: Pull dvc data =============\n"
 pip install dvc[s3]
-dvc pull --force $data_dir/quantization/gpt-j.dvc
-RELEASED_QUANT_PARAM_PATH=$data_dir/quantization/gpt-j/calibration_range/quant_param.npy
+dvc pull $data_dir/quantization/llama2-70b.dvc --force
+RELEASED_QUANT_PARAM_PATH=$data_dir/quantization/llama2-70b/calibration_range/quant_param.npy
 
 
 printf "\n============= STEP-3: Check the equivalence of quantiation parameters =============\n"
@@ -74,12 +78,11 @@ printf "\n============= STEP-3: Check the equivalence of quantiation parameters 
 python ci_file/utils/check_qparam_equivalence.py --released_quant_param_path=$RELEASED_QUANT_PARAM_PATH \
                                     --created_quant_param_path=$QUANT_PARAM_PATH\
 
-                                            
-
-
-unset LOG_PATH
-
-printf "\n============= End of calibration =============\n"
+# unset exported env. variables
+unset SCENARIO
+unset DATA_TYPE
+unset N_COUNT
+unset DEVICE
 
 # exit from conda env.
 conda deactivate
