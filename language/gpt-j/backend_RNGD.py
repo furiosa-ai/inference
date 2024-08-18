a664c09ad12459f2503ec9fad1427c0418a8fb25
BeomGeunCho <beomgeun.cho@furiosa.ai>
2024-08-18 14:15:07 +0000

    change quant_kv_dtype

--- language/gpt-j/backend_RNGD.py
+++ language/gpt-j/backend_RNGD.py
@@ -40,7 +40,7 @@ RETURN_DICT_IN_GENERATE = False
 LOGITS_PROCESSOR = MinNewTokensLengthLogitsProcessor
 STOPPING_CRITERIA = MaxLengthCriteria
 KV_DTYPE = torch.float32
-QUANT_KV_DTYPE = torch.int8
+QUANT_KV_DTYPE = torch.float32
 BUCKET_SIZE = 2048
 NUM_REAL_BATCH = 1
 
