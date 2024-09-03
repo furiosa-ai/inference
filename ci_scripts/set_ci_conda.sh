754f973a0b6be341e1c3f300c0f93864bc797afd
Phil <164851794+kphilpark@users.noreply.github.com>
2024-09-03 14:45:16 +0900

    Update set_ci_conda.sh

--- ci_scripts/set_ci_conda.sh
+++ ci_scripts/set_ci_conda.sh
@@ -24,14 +24,16 @@ cd $git_dir/loadgen; python setup.py install
 if pip show transformers > /dev/null 2>&1; then
     echo "transformers is already installed. Replace transformers to furiosa transformers..."
     pip uninstall -y transformers
-    pip install git+https://github.com/furiosa-ai/transformers-comp.git@2b012fcf15006e2cb2b0d9735ebf5b1d08a744a8#egg=transformers
 fi
+pip install git+https://github.com/furiosa-ai/transformers-comp.git@2b012fcf15006e2cb2b0d9735ebf5b1d08a744a8#egg=transformers
+echo "furiosa transformers 설치 완료!."
 
 if pip show accelerate > /dev/null 2>&1; then
     echo "accelerate is already installed. Replace accelerate to furiosa accelerate..."
     pip uninstall -y accelerate
-    pip install git+https://github.com/furiosa-ai/accelerate-compression.git@4d7b404041834d35727064e5b1dcfcd060319ad6#egg=accelerate
 fi
+pip install git+https://github.com/furiosa-ai/accelerate-compression.git@4d7b404041834d35727064e5b1dcfcd060319ad6#egg=accelerate
+echo "furiosa accelerator 설치 완료!."
 
 printf "\n============= conda set done! =============\n"
 
